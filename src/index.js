bb74b57743a75ac6b3480ff5267592891ecb52d5
Joe Farrell <disastrousgamemaster@gmail.com>
2020-05-10 19:39:58 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'React Boilerplate';
+
+ReactDOM.render(
+  <App title={title} />,
+  document.getElementById('app')
+);
